7c074e7b1b7b70746da2ce5b96794734453aa012
NimaAiryana <airyana.nima@gmail.com>
2021-09-10 09:11:55 +0430

    init

---
+++ NCorp.Framework/Middleware/Extensions/Swagger/SwaggerMiddlewareExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace NCorp.Framework.Middleware.Extensions.Swagger
+{
+    public static class SwaggerMiddlewareExtensions
+    {
+        public static void UseSwagger(this IServiceCollection services, string name, string version)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(version, new OpenApiInfo { Title = name, Version = version });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Jwt Authorization",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+        }
+
+        public static void UseSwagger(this IApplicationBuilder app, string serviceName, string version)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint($"/swagger/{version}/swagger.json", $"{serviceName} {version}"));
+        }
+    }
+}
