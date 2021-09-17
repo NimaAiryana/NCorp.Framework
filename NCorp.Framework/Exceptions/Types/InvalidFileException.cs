25f7832556a1d9173831ff6188b469691aeb3996
NimaAiryana <airyana.nima@gmail.com>
2021-09-17 10:51:30 +0430

    InvalidFileException added

---
+++ NCorp.Framework/Exceptions/Types/InvalidFileException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace NCorp.Framework.Exceptions.Types
+{
+    [Serializable]
+    public class InvalidFileException : Exception
+    {
+        public InvalidFileException() : base() { }
+        public InvalidFileException(string mimeType) : base($"File type {mimeType} is not valid!") { }
+        public InvalidFileException(string mimeType, Exception innerException) : base($"File type {mimeType} is not valid!", innerException) { }
+
+        protected InvalidFileException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
