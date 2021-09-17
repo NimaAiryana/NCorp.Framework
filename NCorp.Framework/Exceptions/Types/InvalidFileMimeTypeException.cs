55f5e3ec1ad8a518ebe0d3f21c8814b30867fee1
NimaAiryana <airyana.nima@gmail.com>
2021-09-17 11:53:19 +0430

    InvalidFileException changed to InvalidFileMimeTypeException and InvalidFileSizeException

---
+++ NCorp.Framework/Exceptions/Types/InvalidFileMimeTypeException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace NCorp.Framework.Exceptions.Types
+{
+    [Serializable]
+    public class InvalidFileMimeTypeException : Exception
+    {
+        public InvalidFileMimeTypeException() : base() { }
+        public InvalidFileMimeTypeException(string mimeType) : base($"File type {mimeType} is not valid!") { }
+        public InvalidFileMimeTypeException(string mimeType, Exception innerException) : base($"File type {mimeType} is not valid!", innerException) { }
+
+        protected InvalidFileMimeTypeException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ NCorp.Framework/Exceptions/Types/InvalidFileSizeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace NCorp.Framework.Exceptions.Types
+{
+    [Serializable]
+    public class InvalidFileSizeException : Exception
+    {
+        public InvalidFileSizeException() : base("File size is out of range!") { }
+        public InvalidFileSizeException(Exception innerException) : base("File size is out of range!", innerException) { }
+        public InvalidFileSizeException(string message) : base(message) { }
+        public InvalidFileSizeException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected InvalidFileSizeException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
