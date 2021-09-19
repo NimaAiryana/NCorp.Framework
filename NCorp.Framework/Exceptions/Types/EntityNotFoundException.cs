f13e00f3202094a8528cb5df82c2c51a337a2e91
NimaAiryana <airyana.nima@gmail.com>
2021-09-19 23:12:04 +0430

    EntityNotFoundException

---
+++ NCorp.Framework/Exceptions/Types/EntityNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace NCorp.Framework.Exceptions.Types
+{
+    [Serializable]
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException() : base("The entity not found!") { }
+        public EntityNotFoundException(string message) : base(message) { }
+        public EntityNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected EntityNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
--- NCorp.Framework/Exceptions/Types/InvalidFileSizeException.cs
+++ NCorp.Framework/Exceptions/Types/InvalidFileSizeException.cs
@@ -6,8 +6,8 @@ namespace NCorp.Framework.Exceptions.Types
     [Serializable]
     public class InvalidFileSizeException : Exception
     {
-        public InvalidFileSizeException() : base("File size is out of range!") { }
-        public InvalidFileSizeException(Exception innerException) : base("File size is out of range!", innerException) { }
+        public InvalidFileSizeException() : base("The file size is out of range!") { }
+        public InvalidFileSizeException(Exception innerException) : base("The file size is out of range!", innerException) { }
         public InvalidFileSizeException(string message) : base(message) { }
         public InvalidFileSizeException(string message, Exception innerException) : base(message, innerException) { }
 
